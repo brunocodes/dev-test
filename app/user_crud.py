c21f0fea37b9ec1574aa05229981b1d7af6b34b5
Bruno Batista <bho360@hotmail.com>
2022-05-14 23:25:36 -0400

    Added user_crud models

--- app/user_crud.py
+++ app/user_crud.py
@@ -2,25 +2,25 @@ import time
 from .config import settings
 from . import models, schemas, utils
 from .database import user_collection
-from .models import User
+from .models import UserIn, UserOut
 
-async def fetch_one_user(user_id: str) -> User:
+async def fetch_one_user(user_id: str) -> UserOut:
     document = await user_collection.find_one({"_id": user_id})
     return document
 
-async def fetch_all_users() -> list[User]:
+async def fetch_all_users() -> list[UserOut]:
     users = []
     cursor = user_collection.find({})
     async for document in cursor:
-        users.append(User(**document))
+        users.append(UserOut(**document))
     return users
 
-async def create_user(user: User) -> User:
+async def create_user(user: UserIn) -> UserOut:
     document = user
     result = await user_collection.insert_one(document)
     return document
 
-async def update_user(user_id: str, email: str) -> User:
+async def update_user(user_id: str, email: str) -> UserOut:
     await user_collection.update_one({"_id": user_id}, {"$set": {"email": email}})
     document = await user_collection.find_one({"_id": user_id})
     return document
