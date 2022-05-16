18acf13e791e0fcb690408655ef3ec5033917ca6
Bruno Batista <bho360@hotmail.com>
2022-05-16 00:51:01 -0400

    Implementação inicial do front-end client

---
+++ client/pages/signup.js
@@ -0,0 +1,102 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import { useState } from "react";
+
+export default function Signup() {
+  const [submited, setSubmit] = useState(false);
+  const [clear, setClear] = useState(false);
+  const submitUser = async (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      "user_id": 0,
+      "nome": event.target.nome.value,
+      "email": event.target.email.value,
+      "telefone": event.target.telefone.value,
+      "endereco": event.target.endereco.value,
+      "profissao": event.target.profissao.value
+    };
+    
+    fetch("http://127.0.0.1:8000/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        newUser
+      })
+      
+    }).then((response) => {
+      console.log(newUser)
+      if (response.status === 201 || response.status === 200) return response.json();
+      // console.log(response)
+      throw new Error("Failed to add user");
+
+    }).then((jsonRes) => {
+      setSubmit(true);
+      alert(`You have submited ${event.target.nome.value}`);
+    })
+  };
+  const submitClear = async (event) => {
+    event.preventDefault();
+    setClear(true);
+  }
+  return (
+    <>
+    <nav className={styles.nav}>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <Link href="/#"><button>signup</button></Link>  
+      </ul>
+    </nav>
+    <div className={styles.container}>
+      <Head>
+        <title>Signup</title>
+        <meta name="description" content="Signup now" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.description}>
+          Sign  up Now!
+        </h1>
+        <div>
+
+          <form onSubmit={submitUser}>
+            <label for="nome">Nome Completo</label><br />
+            <input type="text" id="nome" name="nome" /><br />
+
+            <label for="email">Email:</label><br />
+            <input type="text" id="email" name="email" /><br />
+
+            <label for="telefone">Telefone:</label><br />
+            <input type="text" id="telefone" name="telefone" /><br />
+
+            <label for="endereco">Endereco:</label><br />
+            <input type="text" id="endereco" name="endereco" /><br />
+
+            <label for="profissao">Profissao:</label><br />
+            <input type="text" id="profissao" name="profissao" /><br />
+            <button>Sign In</button>
+          </form>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+    </>
+  )
+}
